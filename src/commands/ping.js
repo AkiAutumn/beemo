663e370aea5c45268f2649e6eea0139d28511840
Kumo <danielkuehnel01@gmail.com>
2022-06-30 21:47:48 +0200

    update

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -4,6 +4,6 @@ module.exports = {
     data: new SlashCommandBuilder().setName('ping').setDescription('Pong!'),
     async execute(interaction) {
         var ping = Date.now() - message.createdTimestamp
-        message.reply('**Pong! with a Delay of**\n' + ping + 'ms');
+        interaction.reply('**Pong! with a Delay of**\n' + ping + 'ms');
     }
 }
