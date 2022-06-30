4efa96d347305a73a2c516e2cb17af21db38a340
Kumo <danielkuehnel01@gmail.com>
2022-06-30 21:33:02 +0200

    update

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -3,9 +3,8 @@ const {SlashCommandBuilder} = require('@discordjs/builders');
 module.exports = {
     data: new SlashCommandBuilder().setName('ping').setDescription('Pong!'),
     async execute(interaction) {
-        message.channel.send("Pinging...").then(m =>{
-                        var ping = m.createdTimestamp - message.createdTimestamp;
-                        var botPing = Math.round(bot.pi);
+        m => {   var ping = m.createdTimestamp - message.createdTimestamp;
+                var botPing = Math.round(bot.pi);
         interaction.reply('**Pong! with a Delay of**\n ${ping}ms');
         });
     }
