48a2c01758e02b86a471dc2b2aa2ec851ab3d488
BuildTools <unconfigured@null.spigotmc.org>
2022-07-06 21:20:06 +0200

    fewf

--- src/commands/dice.js
+++ src/commands/dice.js
@@ -3,6 +3,6 @@ const {SlashCommandBuilder} = require('@discordjs/builders');
 module.exports = {
     data: new SlashCommandBuilder().setName('dice').setDescription('diceing :sunglasses: '),
     async execute(interaction) {
-        interaction.reply('You rolled a ' + math.random);
+        interaction.reply('You rrrolled a ' + math.random);
     }
 }
